d40058803e6df1533cf472ad1727063afbeb6aeb
rjsouza <rjsouza1995@gmail.com>
2015-02-18 13:08:01 -0800

    First Edits

--- src/MainCharacter.java
+++ src/MainCharacter.java
@@ -1,4 +1,16 @@
-
 public class MainCharacter {
+
+	public static String charName = "LittleBitch"; // Name of Main Character
+	public int characterHealth;
+
+	public void characterHealth() {
+		characterHealth = 0;
+	}
+
+	public int getHealth() {
+
+		System.out.println("Health =");
+		return characterHealth;
+	}
 
 }
