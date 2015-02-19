058a2847f8bac6a0005ed5ee3cd1df4257a5f04b
RJ Souza <rjsouza1995@gmail.com>
2015-02-18 17:43:44 -0800

    Added methods to MainCharacter
    Worked on adding character health and strength

--- src/MainCharacter.java
+++ src/MainCharacter.java
@@ -2,21 +2,61 @@ public class MainCharacter {
 
 	private String characterName = "LittleBitch";
 	private int characterHealth;
+	private int characterStrength;
 
-	public MainCharacter(int aCharacterHealth) {
-		
-		characterHealth = aCharacterHealth;
+	/**
+	 * Constructs a character with 50 health and 25 strength
+	 */
+	public MainCharacter() {
+
+		setCharacterHealth();
+		setCharacterStrength();
 
 	}
 
 	/**
 	 * Gets the amount of health the character has remaining
+	 * 
 	 * @return the amount of health the character has
 	 */
-	public int getHealth() {
+	public int getCharacterHealth() {
 		System.out.print(characterName);
-		System.out.println("'s Health =");
+		System.out.println("'s Health = " + characterHealth);
+		return characterHealth; // Cannot get this to return the character
+								// health after Systme.out.print statements
+
+	}
+
+	/**
+	 * Gets the strength points of the character
+	 * 
+	 * @return the strength points of the character
+	 */
+	public int getCharacterStrength() {
+		System.out.print(characterName);
+		System.out.println("'s Strength = " + characterStrength);
+		return characterStrength; // Cannot get this to return the character
+									// strength after Systme.out.print
+									// statements
+	}
+
+	/**
+	 * Sets character health to 50 Not sure if this is needed
+	 * 
+	 * @return the health of the character
+	 */
+	public int setCharacterHealth() {
+		this.characterHealth = 50;
 		return characterHealth;
 	}
 
+	/**
+	 * Sets character strength to 25 Not sure if this is needed
+	 * 
+	 * @return the strength of the character
+	 */
+	public int setCharacterStrength() {
+		this.characterStrength = 25;
+		return characterStrength;
+	}
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,8 @@
 public class TestClass {
 	public static void main(String[] args) {
-		MainCharacter tommy = new MainCharacter(15);
-		tommy.getHealth();
+		MainCharacter tommy = new MainCharacter();
+		tommy.getCharacterHealth();
+		tommy.getCharacterStrength();
+
 	}
 }
